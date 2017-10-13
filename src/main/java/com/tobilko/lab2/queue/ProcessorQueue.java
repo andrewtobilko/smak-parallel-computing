db238d6d286009fdf4d4af615e60ec0357c28d05
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-10-13 23:59:39 +0300

    ProcessorQueue - new println

--- src/main/java/com/tobilko/lab2/queue/ProcessorQueue.java
+++ src/main/java/com/tobilko/lab2/queue/ProcessorQueue.java
@@ -4,13 +4,17 @@ import com.tobilko.lab1.generator.RandomGenerator;
 import com.tobilko.lab2.generator.ProcessGenerator;
 import com.tobilko.lab2.process.Process;
 import com.tobilko.lab2.processor.Processor;
+import com.tobilko.lab2.util.ColourUtil;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.SneakyThrows;
 
 import java.util.PriorityQueue;
 
+import static com.tobilko.lab2.util.ColourUtil.*;
+import static com.tobilko.lab2.util.ColourUtil.Color.*;
 import static com.tobilko.lab2.util.Util.generateRandomId;
+import static java.lang.String.format;
 
 /**
  * Created by Andrew Tobilko on 9/25/17.
@@ -22,6 +26,7 @@ public final class ProcessorQueue {
     @Setter
     @Getter
     private Processor processor;
+
     @Getter
     private final PriorityQueue<Process> processes;
 
@@ -57,11 +62,11 @@ public final class ProcessorQueue {
             while (numberOfProcessesToGenerate >= 0) {
                 Process process = generator.generate();
                 long timeInterval = process.getTimeInterval();
-                System.out.printf("Generated a process for %s - %s. The number of processes remaining: %d.\n",
-                        ProcessorQueue.this, process, numberOfProcessesToGenerate--);
+                println(format("Generated a process for %s - %s. The number of processes remaining: %d.\n",
+                        ProcessorQueue.this, process, numberOfProcessesToGenerate--), BLACK);
 
                 synchronized (ProcessorQueue.this.getProcessor()) {
-                    System.out.printf("%s is notifying %s.\n", ProcessorQueue.this, ProcessorQueue.this.getProcessor());
+                    println(format("%s is notifying %s.", ProcessorQueue.this, ProcessorQueue.this.getProcessor()), BLACK);
                     ProcessorQueue.this.getProcessor().notify();
                 }
 
