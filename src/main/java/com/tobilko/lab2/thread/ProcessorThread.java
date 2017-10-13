c0862f5d776d7215dcc5ba2a52fbb25ca49ef046
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-10-14 00:00:13 +0300

    ProcessorThread - new println

--- src/main/java/com/tobilko/lab2/thread/ProcessorThread.java
+++ src/main/java/com/tobilko/lab2/thread/ProcessorThread.java
@@ -1,7 +1,11 @@
 package com.tobilko.lab2.thread;
 
 import com.tobilko.lab2.processor.Processor;
+import com.tobilko.lab2.util.ColourUtil;
 import lombok.RequiredArgsConstructor;
+
+import static com.tobilko.lab2.util.ColourUtil.println;
+import static java.lang.String.format;
 
 /**
  * Created by Andrew Tobilko on 9/26/17.
@@ -13,7 +17,7 @@ public final class ProcessorThread extends Thread {
 
     @Override
     public void run() {
-        System.out.printf("%s is gonna start...\n", processor);
+        println(format("%s is gonna start...\n", processor), ColourUtil.Color.BLACK);
         processor.process();
     }
 
