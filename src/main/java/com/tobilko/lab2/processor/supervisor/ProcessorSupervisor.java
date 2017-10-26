f1e89d974eb3945eab4794f71230be45a5839604
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-10-26 23:35:34 +0300

    lab2

--- src/main/java/com/tobilko/lab2/processor/supervisor/ProcessorSupervisor.java
+++ src/main/java/com/tobilko/lab2/processor/supervisor/ProcessorSupervisor.java
@@ -1,8 +1,5 @@
 package com.tobilko.lab2.processor.supervisor;
 
-import com.tobilko.lab2.Stealer;
-import com.tobilko.lab2.StolenItem;
-import com.tobilko.lab2.StolenProcess;
 import com.tobilko.lab2.process.Process;
 import com.tobilko.lab2.processor.Processor;
 import lombok.Getter;
@@ -11,8 +8,6 @@ import lombok.Setter;
 
 import java.util.Deque;
 import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.function.Consumer;
 
 /**
  * Created by Andrew Tobilko on 10/16/17.
@@ -20,7 +15,7 @@ import java.util.function.Consumer;
 @Getter
 @Setter
 @RequiredArgsConstructor
-public final class ProcessorSupervisor implements Runnable, Stealer<StolenItem<Process, Deque<Process>>> {
+public final class ProcessorSupervisor implements Runnable {
 
     private final Processor processor;
 
@@ -33,40 +28,11 @@ public final class ProcessorSupervisor implements Runnable, Stealer<StolenItem<P
         while (true) {
             if (!deque.isEmpty()) {
                 // todo: execute own task
-
-                handleExceptionTODO(deque.pollFirst(), Throwable::printStackTrace); // todo : can't happen
             } else {
                 // todo : try to steal
-                StolenItem<Process, Deque<Process>> stolenProcess = steal();
-                Process item = stolenProcess.getItem();
-                Deque<Process> stolenFrom = stolenProcess.getStolenFrom();
-
-                handleExceptionTODO(item, e -> {
-                    stolenFrom.addFirst(item);
-                });
             }
         }
 
-    }
-
-    private void handleExceptionTODO(Process process, Consumer<InterruptedException> consumer) {
-        try {
-            processor.process(process);
-        } catch (InterruptedException e) {
-            consumer.accept(e);
-        }
-
-    }
-
-
-    @Override
-    public StolenItem<Process, Deque<Process>> steal() {
-        for (Deque<Process> deque : deques) {
-            if (!deque.isEmpty()) {
-                return StolenProcess.of(deque, deque.pollFirst());
-            }
-        }
-        throw new NoSuchElementException("All the deques are empty...");
     }
 
 }
