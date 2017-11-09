c15b8585137fdcc95784c952cdeba576ae8c780b
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-11-10 01:06:40 +0200

    lab4 - mains

---
+++ src/main/java/com/tobilko/lab4/writer/Application.java
@@ -0,0 +1,12 @@
+package com.tobilko.lab4.writer;
+
+/**
+ * Created by Andrew Tobilko on 11/10/17.
+ */
+public final class Application {
+
+    public static void main(String[] args) {
+
+    }
+
+}
