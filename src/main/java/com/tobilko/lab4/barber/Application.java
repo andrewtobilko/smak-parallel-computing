40848fad185aba34132013caacd544071a7a206e
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-11-13 01:59:08 +0200

    barber app

--- src/main/java/com/tobilko/lab4/barber/Application.java
+++ src/main/java/com/tobilko/lab4/barber/Application.java
@@ -1,8 +1,8 @@
 package com.tobilko.lab4.barber;
 
 import com.tobilko.lab4.barber.entity.Barbershop;
-import com.tobilko.lab4.barber.thread.BarberThread;
-import com.tobilko.lab4.barber.thread.ConsumerThread;
+import com.tobilko.lab4.barber.runnable.BarberRunnable;
+import com.tobilko.lab4.barber.runnable.ConsumerRunnable;
 
 /**
  * Created by Andrew Tobilko on 11/11/17.
@@ -12,8 +12,8 @@ public final class Application {
     public static void main(String[] args) {
         final Barbershop barbershop = Barbershop.createStandardBarbershop();
 
-        new BarberThread(barbershop).start();
-        new ConsumerThread(barbershop).start();
+        new Thread(new BarberRunnable(barbershop)).start();
+        new Thread(new ConsumerRunnable(barbershop)).start();
     }
 
 }
