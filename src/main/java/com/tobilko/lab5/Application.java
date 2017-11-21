ebcc4ce9a493f4b73ed43ef9bd90e0e9312ea7b1
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-11-21 19:00:39 +0200

    gauss non-parallel solution

--- src/main/java/com/tobilko/lab5/Application.java
+++ src/main/java/com/tobilko/lab5/Application.java
@@ -1,10 +1,15 @@
 package com.tobilko.lab5;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 /**
+ * Gauss-Jordan Elimination Method
+ * http://pages.pacificcoast.net/~cazelais/251/gauss-jordan.pdf
+ *
  * Created by Andrew Tobilko on 11/15/17.
  */
 public final class Application {
@@ -20,21 +25,29 @@ public final class Application {
         };
         final int[] rightMatrix = {5, 8, 2};
 
-        printAugmentedMatrix(leftMatrix, rightMatrix,"before");
+        printAugmentedMatrix(leftMatrix, rightMatrix, "before");
 
 
         // 2. Use row operations to transform the augmented matrix in the form described below, which is
         // called the reduced row echelon form (RREF).
 
         for (int rowIndex = 1; rowIndex < leftMatrix.length; ++rowIndex) {
-            for (int columnIndex = 0; columnIndex < leftMatrix[rowIndex].length; ++columnIndex) {
-                leftMatrix[rowIndex][columnIndex] = leftMatrix[rowIndex][columnIndex] - leftMatrix[rowIndex - 1][0] * 2;
+
+            for (int columnIndex = 0; columnIndex < leftMatrix.length; ++columnIndex) {
+                if (leftMatrix[rowIndex][columnIndex] == 0) {
+                    continue;
+                }
+                // R2[i][j] = R2[i][j] + (-K) * R1[i][j]
+                // K = the leftmost nonzero element
+
+                int K = leftMatrix[rowIndex][columnIndex];
+                System.out.println("K = " + K);
+                leftMatrix[rowIndex][columnIndex] = leftMatrix[rowIndex][columnIndex] - K * leftMatrix[rowIndex - 1][columnIndex];
             }
-            printAugmentedMatrix(leftMatrix, rightMatrix,"step " + rowIndex);
+            printAugmentedMatrix(leftMatrix, rightMatrix, "step " + rowIndex);
         }
 
-
-        printAugmentedMatrix(leftMatrix,rightMatrix, "after");
+        printAugmentedMatrix(leftMatrix, rightMatrix, "after");
     }
 
     public static void printAugmentedMatrix(int[][] matrix, int[] row, String title) {
