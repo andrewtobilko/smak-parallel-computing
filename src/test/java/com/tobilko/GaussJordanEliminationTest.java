f6ac84009a5c415bbbd5e98d532dccf5f7853388
Andrew Tobilko <andrew.tobilko@gmail.com>
2017-11-22 12:47:04 +0200

    algorithm refactored

--- src/test/java/com/tobilko/GaussJordanEliminationTest.java
+++ src/test/java/com/tobilko/GaussJordanEliminationTest.java
@@ -41,45 +41,7 @@ public final class GaussJordanEliminationTest {
                 {0, 2, 1, 0, 4},
                 {-4, 6, 0, 0, 7},
         };
-        double[] b = {4, 4, 9, -6, 5};
-
-        test(matrix, b);
-    }
-
-    @Test
-    public void test4() {
-        double[][] matrix = {
-                {2, -3, -1, 2, 3},
-                {4, -4, -1, 4, 11},
-                {2, -5, -2, 2, -1},
-                {0, 2, 1, 0, 4},
-                {-4, 6, 0, 0, 7},
-        };
         double[] b = {4, 4, 9, -5, 5};
-
-        test(matrix, b);
-    }
-
-    @Test
-    public void test5() {
-        double[][] matrix = {
-                {2, -1, 1},
-                {3, 2, -4},
-                {-6, 3, -3},
-        };
-        double[] b = {1, 4, 2};
-
-        test(matrix, b);
-    }
-
-    @Test
-    public void test6() {
-        double[][] matrix = {
-                {1, -1, 2},
-                {4, 4, -2},
-                {-2, 2, -4},
-        };
-        double[] b = {-3, 1, 6};
 
         test(matrix, b);
     }
@@ -87,16 +49,11 @@ public final class GaussJordanEliminationTest {
     private void test(double[][] A, double[] b) {
         final GaussJordanElimination gaussian = new GaussJordanElimination(A, b);
 
-        if (gaussian.isFeasible()) {
-            printResultWithTitle(gaussian.primal(), "Solution to Ax = b");
-        } else {
-            printResultWithTitle(gaussian.dual(), "Certificate of infeasibility");
-        }
+        System.out.println("Solution to Ax = b");
+        printResult(gaussian.extractSolution());
     }
 
-    private void printResultWithTitle(double[] result, String title) {
-        System.out.println(title);
-
+    private void printResult(double[] result) {
         for (double i : result) {
             System.out.printf("%10.3f\n", i);
         }
